cbab297a5be9d0dce3b0a00b2766f377d739b384
Leandro Gonzalez Matkovich <leandrogonzalezmat@gmail.com>
2024-04-08 17:26:20 -0300

    small fix

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -12,7 +12,7 @@ const Results = async ({query,type}) =>
             type == 'movie'
             ?
             <>
-                <h2 style={{textAlign:'center'}} className="p-3 mx-[20%] rounded-md text-white bg-slate-900 text-xl">MOVIE RESULTS FOR <span className="italic text-xl"> '{query}'</span></h2>
+                <h2 style={{textAlign:'center'}} className="p-3 mx-[20%] rounded-md text-white bg-slate-900 text-xl">MOVIE RESULTS FOR <span className="italic text-xl"> {query}</span></h2>
                 <div className="cardgrid">
                 {movieResults?.map((media) =>
                 (<Card media={media} key={media.id} />))}
@@ -20,7 +20,7 @@ const Results = async ({query,type}) =>
             </>
             :
             <>
-                <h2 style={{textAlign:'center'}} className="p-3 mx-[20%] rounded-md text-white bg-slate-900 text-xl">TV-SHOW RESULTS FOR <span className="italic text-xl"> '{query}'</span></h2>
+                <h2 style={{textAlign:'center'}} className="p-3 mx-[20%] rounded-md text-white bg-slate-900 text-xl">TV-SHOW RESULTS FOR <span className="italic text-xl"> {query}</span></h2>
                 <div className="cardgrid">
                 {tvResults?.map((media) =>
                 (<Card media={media} key={media.id} />))}
